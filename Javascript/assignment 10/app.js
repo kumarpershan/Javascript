6c40702d0009be75fd261c5e5b97a71fe1cf54d5
Pershan kumar <kumarpershan9@gmail.com>
2025-05-24 07:30:46 -0700

    Add files via upload
    assignment 10 js

---
+++ Javascript/assignment 10/app.js
@@ -0,0 +1,52 @@
+//  QUESTION # 2
+
+// document.getElementById("mobile1").addEventListener("click", function() {
+//     alert("Thank You for purchasing a phone from us");
+// });
+
+// document.getElementById("mobile2").addEventListener("click", function() {
+//     alert("Thank You for purchasing a phone from us");
+// });
+
+// document.getElementById("mobile3").addEventListener("click", function() {
+//     alert("Thank You for purchasing a phone from us");
+// });
+
+
+
+// QUESTION NO 3
+
+// var getDevare = document.querySelector("#del")
+
+// function  del(btn){
+//      btn.parentNode.remove()
+// }
+
+
+
+
+// QUESTION # 4
+
+
+//   function changeImage() {
+//     document.getElementById('myImage').src = 'image2.jpg';
+//   }
+
+//   function resetImage() {
+//     document.getElementById('myImage').src = 'image1.jpg';
+//   }
+
+
+// QUESTION # 5
+
+// var count = 0;
+
+//   function increase() {
+//     count++;
+//     document.getElementById('counter').textContent = count;
+//   }
+
+//   function decrease() {
+//     count--;
+//     document.getElementById('counter').textContent = count;
+//   }
